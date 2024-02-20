8d10c95c8993ed674bd9516d89ac18ebf942b8bd
sadekcric4 <sadekcric4@gmail.com>
2024-02-20 20:32:15 +0600

    history API added

---
+++ script.js
@@ -0,0 +1,14 @@
+function back(){
+  window.history.back();
+}
+function forward(){
+  window.history.forward();
+}
+
+function backTo(n){
+  window.history.go(n);
+}
+
+function getHistory(){
+  alert(window.history.length)
+}
